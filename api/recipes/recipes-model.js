c94df5707510e09f55ec4cfbc1d0df0c013e1111
Jonathan Fox <FoxJonathanP@gmail.com>
2021-04-15 16:27:02 -0700

    added getRecipeById function

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,26 @@
+const db = require('../../data/db-config');
+
+async function getRecipeById(recipe_id) {
+  const recipe = await db('recipes').where({ id: recipe_id }).first();
+  const steps = await db('steps').where({ recipe_id });
+
+  const stepIngredients = await Promise.all(
+    steps.map(async step => (
+      db('stepIngredients as si')
+        .where('si.step_id', step.id)
+        .leftJoin('ingredients as in', 'si.ingredient_id', 'in.id')
+        .select('quantity', 'name')
+    ))
+  );
+
+  steps.forEach((step, i) => {
+    step.ingredients = stepIngredients[i];
+  });
+
+  recipe.steps = steps;
+  return Promise.resolve(recipe);
+}
+
+module.exports = {
+  getRecipeById
+}
